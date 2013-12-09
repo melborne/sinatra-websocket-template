2750e276f8b7ac91095e44f31e39394878b0e70b
kyoendo <postagie@gmail.com>
2013-12-09 10:12:47 +0900

    init

---
+++ lib/sinatra_websocket_template/version.rb
@@ -0,0 +1,3 @@
+module SinatraWebsocketTemplate
+  VERSION = "0.0.1"
+end
---
+++ spec/sinatra_websocket_template_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SinatraWebsocketTemplate do
+  it 'should have a version number' do
+    SinatraWebsocketTemplate::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
