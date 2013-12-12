f3401f369694cebb8766dbbbe6ec56fb10683940
kyoendo <postagie@gmail.com>
2013-12-12 17:46:46 +0900

    Fix when bundle gem command options passed

--- lib/sinatra_websocket_template/cli.rb
+++ lib/sinatra_websocket_template/cli.rb
@@ -12,7 +12,7 @@ module SinatraWebsocketTemplate
     desc "new PROJECT", "Creates a skelton for your project"
     option :dir, aliases:"-d", default:Dir.pwd, banner:"Specify a target directory for your project."
     option :test, aliases:"-t", lazy_default:"rspec", banner:"Generate a test directory(default: rspec)."
-    option :bin, aliases:"-b", default:false, banner:"Generate a binary."
+    option :bin, aliases:"-b", type: :boolean, default:false, banner:"Generate a binary."
     def new(project_name)
       target = File.join(options[:dir], project_name)
       opts = {
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -16,33 +16,49 @@ describe SinatraWebsocketTemplate::CLI do
   end
 
   describe '#new' do
-    it 'creates a skelton project' do
-      described_class.start(['new', @project_name, '--dir', @tmp_path])
-      expect(@project.join("Procfile")).to exist
+    context 'without bundler gem options' do
+      it 'creates a skelton project' do
+        described_class.start(['new', @project_name, '--dir', @tmp_path])
+        expect(@project.join("Procfile")).to exist
 
-      configru = @project.join("config.ru")
-      expect(configru).to exist
-      expect(configru.read).to match /project_a\/backend.*ProjectA::App/m
+        configru = @project.join("config.ru")
+        expect(configru).to exist
+        expect(configru.read).to match /project_a\/backend.*ProjectA::App/m
 
-      apprb = @project.join("lib", "app.rb")
-      expect(apprb).to exist
-      expect(apprb.read).to match /module ProjectA/
+        apprb = @project.join("lib", "app.rb")
+        expect(apprb).to exist
+        expect(apprb.read).to match /module ProjectA/
 
-      indexhaml = @project.join("lib", "views", "index.haml")
-      expect(indexhaml).to exist
-      expect(indexhaml.read).to match /%title ProjectA/
+        indexhaml = @project.join("lib", "views", "index.haml")
+        expect(indexhaml).to exist
+        expect(indexhaml.read).to match /%title ProjectA/
 
-      expect(@project.join("lib", "public", "css", "main.css")).to exist
-      expect(@project.join("lib", "public", "js", "application.js")).to exist
-      expect(@project.join("lib", "project_a", "backend.rb")).to exist
+        expect(@project.join("lib", "public", "css", "main.css")).to exist
+        expect(@project.join("lib", "public", "js", "application.js")).to exist
+        expect(@project.join("lib", "project_a", "backend.rb")).to exist
 
-      # created by 'bundle gem command'
-      expect(@project.join("Gemfile")).to exist
-      expect(@project.join("lib", "project_a.rb")).to exist
+        # created by 'bundle gem command'
+        expect(@project.join("Gemfile")).to exist
+        expect(@project.join("lib", "project_a.rb")).to exist
 
-      gemspec = @project.join("project_a.gemspec")
-      expect(gemspec).to exist
-      expect(gemspec.read).to match /spec\.add_dependency.*faye-websocket.*puma.*sinatra.*haml/m
+        gemspec = @project.join("project_a.gemspec")
+        expect(gemspec).to exist
+        expect(gemspec.read).to match /spec\.add_dependency.*faye-websocket.*puma.*sinatra.*haml/m
+      end
     end
+
+    context 'with bundler gem options' do
+      it 'create a skelton with a binary and spec files' do
+        described_class.start(['new', @project_name, '--dir', @tmp_path, '-bt'])
+        expect(@project.join("bin", "project_a")).to exist
+        expect(@project.join("spec", "project_a_spec.rb")).to exist
+      end
+
+      it 'create a skelton with minitest' do
+        described_class.start(['new', @project_name, '--dir', @tmp_path, '--test=minitest'])
+        expect(@project.join("test", "test_project_a.rb")).to exist
+      end
+    end
+
   end
 end
